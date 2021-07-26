0457b5c119e7a74009709d32653b5d5b594082e3
jasethomson <jasethomson31@gmail.com>
2021-07-25 23:16:51 -0700

    added seo, print and social content. added images needed for work page. updated scss

--- app/pages/work.jsx
+++ app/pages/work.jsx
@@ -8,14 +8,172 @@ const Work = props => {
         <div className="insert-101 insert-101-a">
           <Header title="SEO" />
         </div>
+        <div className="insert-101 insert-101-b">
+          Combining SEO and product knowledge to capture more conversions.
+        </div>
+        <div className="insert-101 insert-101-c">
+          <div className="insert-102 insert-102-a">
+            <div className="insert-103 insert-103-a">
+              <a
+                href="https://www.amazon.com/dp/B07BLTPVN9?pd_rd_i=B07BLTPVN9&pd_rd_w=XSOv2&pf_rd_p=ee186ce0-6bf7-4893-85b9-f3368b019e0f&pd_rd_wg=w7SE9&pf_rd_r=193RNGC13W5RFD866270&pd_rd_r=98629f92-8e6f-4207-bb0d-5e8ddd200f7d"
+                target="_blank">
+                Amazon A+ Content Copy - Artificial Rose Heads (click here for more details)
+            </a>
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/roseheads.png" alt="Amazon A+ Content Rose Heads" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/roseheadsCopy.png" alt="Amazon A+ Content Rose Heads" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-b">
+            <div className="insert-103 insert-103-a">
+              <a
+                href="https://www.amazon.com/Kraft-Notebook-Bulk-Different-Happiness/dp/B079JHHYGT/ref=sr_1_15?dchild=1&keywords=inspirational+notebooks&qid=1627274507&sr=8-15"
+                target="_blank">
+                Amazon A+ Content Copy - Notebooks (click here for more details)
+            </a>
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/notebooks.png" alt="Amazon A+ Notebooks" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/notebooksCopy.png" alt="Amazon A+ Notebooks" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-c">
+            <div className="insert-103 insert-103-a">
+              <a
+                href="https://www.amazon.com/Vintage-Organizer-Supplies-Notions-Rectangular/dp/B01HJKRE1Q?ref_=ast_sto_dp"
+                target="_blank">
+                Juvo Plus Product Description Page - Sewing Basket (click here for more details)
+              </a>
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/sewingBasket.png" alt="Juvo Plus Product Description Page - Sewing Basket" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-d">
+            <div className="insert-103 insert-103-a">
+              Torrid Paid Ads
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/torridPaidAds.png" alt="Torrid Paid Ads First Example" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/torridPaidAds2.png" alt="Torrid Paid Ads Second Example" />
+              </div>
+              <div className="insert-104 insert-104-c">
+                <img src="images/torridPaidAds3.png" alt="Torrid Paid Ads Third Example" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-e">
+            <div className="insert-103 insert-103-a">
+              Torrid Product Description Pages (PDPs)
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/torridPdp1.png" alt="Torrid PDP First Example" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/torridPdp2.png" alt="Torrid PDP Second Example" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-f">
+            <div className="insert-103 insert-103-a">
+              Unique Vintage Product Description Pages (PDPs)
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/uvPdp1.png" alt="Unique Vintage PDP First Example" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/uvPdp2.png" alt="Unique Vintage PDP Second Example" />
+              </div>
+              <div className="insert-104 insert-104-c">
+                <img src="images/uvPdp3.png" alt="Unique Vintage PDP Third Example" />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section id="print" className="sec sec-2">
-        <Header title="Print" />
+        <div className="insert-101 insert-101-a">
+          <Header title="Print" />
+        </div>
+        <div className="insert-101 insert-101-b">
+          Tangible brand marketing that calls for creativity and collective collaboration.
+        </div>
+        <div className="insert-101 insert-101-c">
+          <div className="insert-102 insert-102-a">
+            <div className="insert-103 insert-103-a">
+              Torrid Spring 2020 Mailer
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/torridMailer1.png" alt="Torrid Spring 2020 Mailer First Example" />
+              </div>
+              <div className="insert-104 insert-104-b">
+                <img src="images/torridMailer2.png" alt="Torrid Spring 2020 Mailer Second Example" />
+              </div>
+              <div className="insert-104 insert-104-c">
+                <img src="images/torridMailer3.png" alt="Torrid Spring 2020 Mailer Third Example" />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <section id="social" className="sec sec-3">
-        <Header title="Social" />
+        <div className="insert-101 insert-101-a">
+          <Header title="Social" />
+        </div>
+        <div className="insert-101 insert-101-b">
+          Looking for creative and strategic social content? Copy that.
+        </div>
+        <div className="insert-101 insert-101-c">
+          <div className="insert-102 insert-102-a">
+            <div className="insert-103 insert-103-a">
+              Torrid Email - Star Wars
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/torridStarWars.png" alt="Torrid Email - Star Wars" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-b">
+            <div className="insert-103 insert-103-a">
+              Torrid Instagram Story - Cinderella
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/torridCinderella.png" alt="Torrid Instagram Story - Cinderella" />
+              </div>
+            </div>
+          </div>
+          <div className="insert-102 insert-102-c">
+            <div className="insert-103 insert-103-a">
+              Torrid Instagram Story - Birds of Prey
+            </div>
+            <div className="insert-103 insert-103-b">
+              <div className="insert-104 insert-104-a">
+                <img src="images/birdsOfPrey.png" alt="Torrid Instagram Story - Birds of Prey" />
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
-    </div>
+    </div >
   );
 }
 
